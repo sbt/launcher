jenshalm/sbt

Launcher can now load servers in addition to regular applications.
* Add new ServerMain interface
* AppProvider can now choose to load ServerMain or AppMain classes
* Explicitly document what happens if something doesn't match an
  expected interface
* Improve error message on inability to load something.
* Parse new [server] section that denotes a service and is lock file
* Ability to serialize launch configurations.
* Attempt to look for active listening server via the lock file
* Forks the launcher itself to run servers from serialized launch configuration.
* Testing echo server.
* Tests to detect basic server functionality will work.
* Revamp all the documentation for the launcher, giving it its own section.
* Full documentation on launcher configuration files.

Revamp launcher documentation to be a bit more in-depth, and split bits into sections.

launch/src/main/scala/xsbt/boot/Launch.scala
@@ -6,20 +6,85 @@ package xsbt.boot
 import Pre._
 import BootConfiguration.{CompilerModuleName, JAnsiVersion, LibraryModuleName}
 import java.io.File
-import java.net.{URL, URLClassLoader}
+import java.net.{URL, URLClassLoader, URI}
 import java.util.concurrent.Callable
 import scala.collection.immutable.List
 import scala.annotation.tailrec
+import ConfigurationStorageState._
 
 object Launch
 {
 	def apply(arguments: List[String]): Option[Int] = apply( (new File("")).getAbsoluteFile , arguments )
 
 	def apply(currentDirectory: File, arguments: List[String]): Option[Int] = {
-		val (configLocation, newArguments) = Configuration.find(arguments, currentDirectory) 
-		val config = parseAndInitializeConfig(configLocation, currentDirectory)  
-		launch(run(Launcher(config)))(makeRunConfig(currentDirectory, config, newArguments))  
+	    val (isLocate, newArgs) = handleLocateFlag(arguments)
+		val (configLocation, newArgs2, state) = Configuration.find(newArgs, currentDirectory)
+		val config = state match {
+		  case SerializedFile => LaunchConfiguration.restore(configLocation)
+		  case PropertiesFile => parseAndInitializeConfig(configLocation, currentDirectory)  
+		}
+	    if(isLocate) {
+	      if(!newArgs2.isEmpty) {
+ 	         // TODO - Print the arguments without exploding proguard size.
+	         System.err.println("Warning: --locate option ignores arguments.")
+	      }
+	      locate(currentDirectory, config)
+	    } else {
+	      // First check to see if there are java system properties we need to set. Then launch the application.
+	      updateProperties(config)
+	      launch(run(Launcher(config)))(makeRunConfig(currentDirectory, config, newArgs2))  
+	    }
 	}
+	/** Check to see if this is a "lookup the server" request. */
+	def handleLocateFlag(args: List[String]): (Boolean, List[String]) =
+	  args match {
+	    case ("-locate" | "--locate") :: tail => (true, tail)
+	    case _ =>  (false, args)
+	  }
+	/** Locate a server, print where it is, and exit. */
+	def locate(currentDirectory: File, config: LaunchConfiguration): Option[Int] = {
+	  locateUri(currentDirectory, config) match {
+	    case Some(uri) =>
+	      // Print out the URI
+	      System.out.println(uri.toASCIIString)
+	      Some(0)
+	    case None =>
+          sys.error(s"${config.app.groupID}-${config.app.main} is not configured as a server.")
+	  }
+
+	}
+	/** Locate the URI of a server with the given launch configuration. */
+	def locateUri(currentDirectory: File, config: LaunchConfiguration): Option[URI] =
+	  config.serverConfig match {
+	  	case Some(_) => Some(ServerLocator.locate(currentDirectory, config))
+	    case None => None
+	  }
+	/** Some hackery to allow sys.props to be configured via a file. If this launch config has
+	 * a valid file configured, we load the properties and and apply them to this jvm.
+	 */
+	def updateProperties(config: LaunchConfiguration): Unit = {
+	  // TODO - give a nice error message on IO failure.
+	  config.serverConfig match {
+	    case Some(config) =>
+	      config.jvmPropsFile match {
+	        case Some(file) if file.exists =>
+	          // Now we load the file.
+	          val props = new java.util.Properties
+	          val reader = new java.io.FileReader(file)
+	          try props.load(reader)
+	          finally reader.close()
+	          // Using java APIs in limited launcher environment => ugly code.
+	          val nameItr = props.stringPropertyNames.iterator
+	          while(nameItr.hasNext) {
+	            val propName = nameItr.next
+	            sys.props(propName) = props.getProperty(propName)
+	          }
+	        case _ =>
+	      }
+	    case None =>
+	  }
+	}
+	
 	/** Parses the configuration *and* runs the initialization code that will remove variable references. */
 	def parseAndInitializeConfig(configLocation: URL, currentDirectory: File): LaunchConfiguration =
 	{
@@ -84,6 +149,10 @@ object Launch
 		Thread.currentThread.setContextClassLoader(loader)
 		try { eval } finally { Thread.currentThread.setContextClassLoader(oldLoader) }
 	}
+	
+	// Cache of classes for lookup later.
+	val ServerMainClass = classOf[xsbti.ServerMain]
+	val AppMainClass = classOf[xsbti.AppMain]
 }
 final class RunConfiguration(val scalaVersion: Option[String], val app: xsbti.ApplicationID, val workingDirectory: File, val arguments: List[String])
 
@@ -240,27 +309,32 @@ class Launch private[xsbt](val bootDirectory: File, val lockBoot: Boolean, val i
 		(scalaHome, libDirectory)
 	}
 
-	def appProvider(appID: xsbti.ApplicationID, app: RetrievedModule, scalaProvider0: xsbti.ScalaProvider, appHome: File): xsbti.AppProvider = new xsbti.AppProvider
-	{
+	def appProvider(appID: xsbti.ApplicationID, app: RetrievedModule, scalaProvider0: xsbti.ScalaProvider, appHome: File): xsbti.AppProvider = 
+	  new xsbti.AppProvider {
+	    import Launch.{ServerMainClass,AppMainClass}
 		val scalaProvider = scalaProvider0
 		val id = appID
 		def mainClasspath = app.fullClasspath
 		lazy val loader = app.createLoader(scalaProvider.loader)
+		// TODO - For some reason we can't call this from vanilla scala.  We get a 
+		// no such method exception UNLESS we're in the same project.
 		lazy val entryPoint: Class[T] forSome { type T } =
 		{
 			val c = Class.forName(id.mainClass, true, loader)
 			if(classOf[xsbti.AppMain].isAssignableFrom(c)) c
 			else if(PlainApplication.isPlainApplication(c)) c
-			else sys.error(s"Class: ${c} is not an instance of xsbti.AppMain nor does it have one of these static methods:\n"+
-			                  " * void main(String[] args)\n * int main(String[] args)\n * xsbti.Exit main(String[] args)")
+			else if(ServerApplication.isServerApplication(c)) c
+			else sys.error(s"${c} is not an instance of xsbti.AppMain, xsbti.ServerMain nor does it have one of these static methods:\n"+
+			               " * void main(String[] args)\n * int main(String[] args)\n * xsbti.Exit main(String[] args)\n")
 		}
 		// Deprecated API.  Remove when we can.
-		def mainClass: Class[T] forSome { type T <: xsbti.AppMain } = entryPoint.asSubclass(classOf[xsbti.AppMain])
+		def mainClass: Class[T] forSome { type T <: xsbti.AppMain } = entryPoint.asSubclass(AppMainClass)
 		def newMain(): xsbti.AppMain = {
-		  if(PlainApplication.isPlainApplication(entryPoint)) PlainApplication(entryPoint)
-		  else mainClass.newInstance
+		  if(ServerApplication.isServerApplication(entryPoint)) ServerApplication(this)
+		  else if(PlainApplication.isPlainApplication(entryPoint)) PlainApplication(entryPoint)
+		  else if(AppMainClass.isAssignableFrom(entryPoint)) mainClass.newInstance
+		  else throw new IncompatibleClassChangeError(s"Main class ${entryPoint.getName} is not an instance of xsbti.AppMain, xsbti.ServerMain nor does it have a valid `main` method.")
 		}
-
 		lazy val components = componentProvider(appHome)
 	}
 	def componentProvider(appHome: File) = new ComponentProvider(appHome, lockBoot)

sbt/launcher

src/main/scala/xsbt/boot/Launch.scala
@@ -6,20 +6,64 @@ package xsbt.boot
 import Pre._
 import BootConfiguration.{CompilerModuleName, JAnsiVersion, LibraryModuleName}
 import java.io.File
-import java.net.{URL, URLClassLoader}
+import java.net.{URL, URLClassLoader, URI}
 import java.util.concurrent.Callable
 import scala.collection.immutable.List
 import scala.annotation.tailrec
+import ConfigurationStorageState._
+
+class LauncherArguments(val args: List[String], val isLocate: Boolean)
 
 object Launch
 {
-	def apply(arguments: List[String]): Option[Int] = apply( (new File("")).getAbsoluteFile , arguments )
+	def apply(arguments: LauncherArguments): Option[Int] = apply( (new File("")).getAbsoluteFile , arguments )
 
-	def apply(currentDirectory: File, arguments: List[String]): Option[Int] = {
-		val (configLocation, newArguments) = Configuration.find(arguments, currentDirectory) 
-		val config = parseAndInitializeConfig(configLocation, currentDirectory)  
-		launch(run(Launcher(config)))(makeRunConfig(currentDirectory, config, newArguments))  
+	def apply(currentDirectory: File, arguments: LauncherArguments): Option[Int] = {
+		val (configLocation, newArgs2, state) = Configuration.find(arguments.args, currentDirectory)
+		val config = state match {
+		  case SerializedFile => LaunchConfiguration.restore(configLocation)
+		  case PropertiesFile => parseAndInitializeConfig(configLocation, currentDirectory)  
+		}
+	    if(arguments.isLocate) {
+	      if(!newArgs2.isEmpty) {
+ 	         // TODO - Print the arguments without exploding proguard size.
+	         System.err.println("Warning: --locate option ignores arguments.")
+	      }
+	      locate(currentDirectory, config)
+	    } else {
+	      // First check to see if there are java system properties we need to set. Then launch the application.
+	      updateProperties(config)
+	      launch(run(Launcher(config)))(makeRunConfig(currentDirectory, config, newArgs2))  
+	    }
 	}
+	/** Locate a server, print where it is, and exit. */
+	def locate(currentDirectory: File, config: LaunchConfiguration): Option[Int] = {
+	  config.serverConfig match {
+	  	case Some(_) => 
+	  	  val uri = ServerLocator.locate(currentDirectory, config)
+	  	  System.out.println(uri.toASCIIString)
+	  	  Some(0)
+	    case None => sys.error(s"${config.app.groupID}-${config.app.main} is not configured as a server.")
+	  }
+	}
+	/** Some hackery to allow sys.props to be configured via a file. If this launch config has
+	 * a valid file configured, we load the properties and and apply them to this jvm.
+	 */
+	def updateProperties(config: LaunchConfiguration): Unit = {
+	  config.serverConfig match {
+	    case Some(config) =>
+	      config.jvmPropsFile match {
+	        case Some(file) if file.exists =>
+	          try setSystemProperties(readProperties(file))
+	          catch {
+	            case e: Exception => throw new RuntimeException(s"Unable to load server properties file: ${file}", e)
+	          }
+	        case _ =>
+	    }
+	    case None =>
+	  }
+	}
+	
 	/** Parses the configuration *and* runs the initialization code that will remove variable references. */
 	def parseAndInitializeConfig(configLocation: URL, currentDirectory: File): LaunchConfiguration =
 	{
@@ -84,6 +128,10 @@ object Launch
 		Thread.currentThread.setContextClassLoader(loader)
 		try { eval } finally { Thread.currentThread.setContextClassLoader(oldLoader) }
 	}
+	
+	// Cache of classes for lookup later.
+	val ServerMainClass = classOf[xsbti.ServerMain]
+	val AppMainClass = classOf[xsbti.AppMain]
 }
 final class RunConfiguration(val scalaVersion: Option[String], val app: xsbti.ApplicationID, val workingDirectory: File, val arguments: List[String])
 
@@ -240,27 +288,32 @@ class Launch private[xsbt](val bootDirectory: File, val lockBoot: Boolean, val i
 		(scalaHome, libDirectory)
 	}
 
-	def appProvider(appID: xsbti.ApplicationID, app: RetrievedModule, scalaProvider0: xsbti.ScalaProvider, appHome: File): xsbti.AppProvider = new xsbti.AppProvider
-	{
+	def appProvider(appID: xsbti.ApplicationID, app: RetrievedModule, scalaProvider0: xsbti.ScalaProvider, appHome: File): xsbti.AppProvider = 
+	  new xsbti.AppProvider {
+	    import Launch.{ServerMainClass,AppMainClass}
 		val scalaProvider = scalaProvider0
 		val id = appID
 		def mainClasspath = app.fullClasspath
 		lazy val loader = app.createLoader(scalaProvider.loader)
+		// TODO - For some reason we can't call this from vanilla scala.  We get a 
+		// no such method exception UNLESS we're in the same project.
 		lazy val entryPoint: Class[T] forSome { type T } =
 		{
 			val c = Class.forName(id.mainClass, true, loader)
 			if(classOf[xsbti.AppMain].isAssignableFrom(c)) c
 			else if(PlainApplication.isPlainApplication(c)) c
-			else sys.error(s"Class: ${c} is not an instance of xsbti.AppMain nor does it have one of these static methods:\n"+
-			                  " * void main(String[] args)\n * int main(String[] args)\n * xsbti.Exit main(String[] args)")
+			else if(ServerApplication.isServerApplication(c)) c
+			else sys.error(s"${c} is not an instance of xsbti.AppMain, xsbti.ServerMain nor does it have one of these static methods:\n"+
+			               " * void main(String[] args)\n * int main(String[] args)\n * xsbti.Exit main(String[] args)\n")
 		}
 		// Deprecated API.  Remove when we can.
-		def mainClass: Class[T] forSome { type T <: xsbti.AppMain } = entryPoint.asSubclass(classOf[xsbti.AppMain])
+		def mainClass: Class[T] forSome { type T <: xsbti.AppMain } = entryPoint.asSubclass(AppMainClass)
 		def newMain(): xsbti.AppMain = {
-		  if(PlainApplication.isPlainApplication(entryPoint)) PlainApplication(entryPoint)
-		  else mainClass.newInstance
+		  if(ServerApplication.isServerApplication(entryPoint)) ServerApplication(this)
+		  else if(PlainApplication.isPlainApplication(entryPoint)) PlainApplication(entryPoint)
+		  else if(AppMainClass.isAssignableFrom(entryPoint)) mainClass.newInstance
+		  else throw new IncompatibleClassChangeError(s"Main class ${entryPoint.getName} is not an instance of xsbti.AppMain, xsbti.ServerMain nor does it have a valid `main` method.")
 		}
-
 		lazy val components = componentProvider(appHome)
 	}
 	def componentProvider(appHome: File) = new ComponentProvider(appHome, lockBoot)