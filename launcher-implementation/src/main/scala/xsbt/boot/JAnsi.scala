96dbfb17aa0546be974231e779ec4101ff8cb8e0
Adriaan Moors <adriaan@lightbend.com>
2017-04-07 14:56:16 -0700

    Reduce output to System.out
    Use System.err instead, which should interfere less
    with the launched application's output

--- launcher-implementation/src/main/scala/xsbt/boot/JAnsi.scala
+++ launcher-implementation/src/main/scala/xsbt/boot/JAnsi.scala
@@ -18,6 +18,6 @@ object JAnsi {
 				* mitigation code that should not render sbt completely unusable if jansi initialization fails.
 				* [From Mark Harrah, https://github.com/sbt/sbt/pull/633#issuecomment-11957578].
 				*/
-      case ex: Throwable                  => println("Jansi found on class path but initialization failed: " + ex)
+      case ex: Throwable                  => System.err.println("Jansi found on class path but initialization failed: " + ex)
     }
 }
