99e17aa5094ef72d82245bf7bdb33342e740168f
Eugene Yokota <eed3si9n@gmail.com>
2022-09-21 16:33:29 -0400

    Fix CI test

--- ci-test/test.sh
+++ ci-test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-LAUNCHER="../../launcher-implementation/target/proguard/launcher-implementation-1.2.0-SNAPSHOT-shading.jar"
+LAUNCHER="../../launcher-implementation/target/proguard/launcher-implementation-1.4.0-SNAPSHOT-shading.jar"
 
 pushd ci-test/app0
 COURSIER_CACHE=/tmp/cache/ java -jar $LAUNCHER @sbt.1.3.13.boot.properties exit
