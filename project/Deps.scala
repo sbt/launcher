28ec17c8b35500e57e7aeb30769c823f1719ae68
kenji yoshida <6b656e6a69@gmail.com>
2026-08-29 13:38:20 +0900

    Update dependencies (#149)

--- project/Deps.scala
+++ project/Deps.scala
@@ -3,8 +3,8 @@ import sbt.Keys.*
 
 object Deps {
   def lib(m: ModuleID) = libraryDependencies += m
-  lazy val sbtIo = "org.scala-sbt" %% "io" % "1.10.5"
-  lazy val scalacheck = "org.scalacheck" %% "scalacheck" % "1.19.0"
+  lazy val sbtIo = "org.scala-sbt" %% "io" % "1.13.0"
+  lazy val scalacheck = "org.scalacheck" %% "scalacheck" % "1.20.0"
   lazy val junit = "junit" % "junit" % "4.13.2"
   lazy val verify = "com.eed3si9n.verify" %% "verify" % "1.0.0"
 
