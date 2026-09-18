e38d489b2a1e7c3d39f580340650625470532257
xuwei-k <6b656e6a69@gmail.com>
2026-09-18 21:03:53 +0900

    Add -Yfuture-lazy-vals. cross build setting

--- ci-test/test.sh
+++ ci-test/test.sh
@@ -1,15 +1,24 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
-LAUNCHER="../../launcher-implementation/target/proguard/launcher-implementation-1.4.0-SNAPSHOT-shading.jar"
+declare -a LAUNCHERS=(
+"../../launcher-implementation/target/jdk8-jvm-3/proguard/launcher-implementation-jdk8-1.4.0-SNAPSHOT-shading.jar"
+"../../launcher-implementation/target/jdk11-jvm-3/proguard/launcher-implementation-1.4.0-SNAPSHOT-shading.jar"
+)
 
-pushd ci-test/app0
-COURSIER_CACHE=/tmp/cache/ java -jar $LAUNCHER @sbt.1.3.13.boot.properties exit
-popd
+for LAUNCHER in "${LAUNCHERS[@]}"
+do
+  pushd ci-test/app0
+  find . -name target -delete || true
+  COURSIER_CACHE=/tmp/cache/ java -jar $LAUNCHER @sbt.1.3.13.boot.properties exit
+  popd
 
-pushd ci-test/app1
-COURSIER_CACHE=/tmp/cache/ java -jar $LAUNCHER @sbt.0.13.18.boot.properties exit
-popd
+  pushd ci-test/app1
+  find . -name target -delete || true
+  COURSIER_CACHE=/tmp/cache/ java -jar $LAUNCHER @sbt.0.13.18.boot.properties exit
+  popd
 
-pushd ci-test/app2
-COURSIER_CACHE=/tmp/cache/ java -jar $LAUNCHER @sbt.1.4.0.boot.properties exit
-popd
+  pushd ci-test/app2
+  find . -name target -delete || true
+  COURSIER_CACHE=/tmp/cache/ java -jar $LAUNCHER @sbt.1.4.0.boot.properties exit
+  popd
+done
